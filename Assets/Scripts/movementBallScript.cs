135863a3562eb36eecdca2fcc98662a8bf7635fc
TechnoChiken379 <luke.vd.tillaar@gmail.com>
2023-09-29 12:48:11 +0200

    update

--- Assets/Scripts/movementBallScript.cs
+++ Assets/Scripts/movementBallScript.cs
@@ -6,16 +6,23 @@ public class movementBallScript : MonoBehaviour
 {
     public float xPosition = 2f;
     public float yPosition = 2f;
+    public float xDirection = 1f;
+    public float yDirection = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-    
+        transform.position = new Vector3(xPosition, yPosition, 0f);   
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(xPosition, yPosition, 0f);    
+        xPosition = xPosition + xDirection * Time.deltaTime;
+        //xPosition += xDirection*Time.deltaTime; = Shorter Version
+        yPosition = xPosition + yDirection * Time.deltaTime;
+        transform.position = new Vector3(xPosition, yPosition, 0f);
+
+        
     }
 }
